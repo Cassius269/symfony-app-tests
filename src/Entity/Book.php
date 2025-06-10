ac69f482751ebc77f38e3186376a6bdc16f5f66a
cassius269 <fahamygaston@gmail.com>
2025-06-10 23:05:21 +0200

    ajout des contraintes de validation sur l'entité Book

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -8,6 +8,8 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 #[ORM\Entity(repositoryClass: BookRepository::class)]
 class Book
 {
@@ -17,9 +19,23 @@ class Book
     private ?int $id = null;
 
     #[ORM\Column(length: 30)]
+    #[Assert\NotBlank(message: 'Le titre du livre est obligatoire')]
+    #[Assert\Length(
+        min: 6,
+        max: 30,
+        minMessage: 'Le titre doit avoir plus de 5 caractères',
+        maxMessage: 'Le titre doit avoir moins de 30 caractères'
+    )]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[Assert\NotBlank(message: 'La description du livre est obligatoire')]
+    #[Assert\Length(
+        min: 30,
+        max: 100,
+        minMessage: 'La description du livre doit avoir plus de 30 caractères',
+        maxMessage: 'La description du livre doit avoir moins de 100 caractères'
+    )]
     private ?string $description = null;
 
     /**
